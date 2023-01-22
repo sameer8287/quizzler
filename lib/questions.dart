5942a8a4a83f6f29c07376ef910c0b6221dcb73a
sergio marquina <sameeryusuf9312@gamil.com>
2023-01-22 14:35:36 +0530

    initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,10 @@
+class Question{
+
+  late String questionText;
+  late bool questionAnswer;
+  Question({ required String q, required bool a})
+  {
+    questionText =q;
+    questionAnswer = a;
+  }
+}
